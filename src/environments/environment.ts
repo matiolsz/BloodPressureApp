07701efb947864674f656fe15d59f79eec3fe60b
matiolsz <matiolsz@wp.pl>
2025-02-22 01:04:28 +0700

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
-
 export const environment = {
     production: false,
     firebaseConfig: {
-        // apiKey: '[API_KEY]',
+        apiKey: API_KEY,
         authDomain: "bloodpressureapp-c1e59.firebaseapp.com",
         projectId: "bloodpressureapp-c1e59",
         storageBucket: "bloodpressureapp-c1e59.firebasestorage.app",
-        // messagingSenderId: '[MESSAGING_SENDER_ID]',
-        // appId: '[APP_ID]',
-        // measurementId: '[MEASUREMENT_ID]'
+        messagingSenderId: MESSAGING_SENDER_ID,
+        appId: APP_ID,
+        measurementId: MEASUREMENT_ID
     } 
 };
