472e584f44368995a43d0aa27d43d6b5d8f02b68
matiolsz <matiolsz@wp.pl>
2025-02-23 23:22:24 +0700

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,9 @@
+
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: process.env["API_KEY"],
         authDomain: "bloodpressureapp-c1e59.firebaseapp.com",
         projectId: "bloodpressureapp-c1e59",
         storageBucket: "bloodpressureapp-c1e59.firebasestorage.app",
-        messagingSenderId: process.env["MESSAGING_SENDER_ID"],
-        appId: process.env["APP_ID"],
-        measurementId: process.env["MEASUREMENT_ID"]
     } 
 };
